cb7a9d8af70dae8ce4e8aba54ed8f4a3deece7fb
Florian Isopp <florian.johannes.isopp@gmail.com>
2022-03-01 14:51:16 +0000

    sql connector base

---
+++ SqlConsole/SqlConsole/SqlConnector.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqlConsole
+{
+    public class SqlConnector
+    {
+        public static void ReadCarData()
+        {
+            string connectionString = "Data Source=VMWM\\SQLEXPRESS;Initial Catalog=restDB;Integrated Security=SSPI";
+            string queryString = "SELECT OrderID, CustomerID FROM dbo.Orders;";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+                connection.Open();
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                // Call Read before accessing data.
+                while (reader.Read())
+                {
+                    ReadSingleRow((IDataRecord)reader);
+                }
+
+                // Call Close when done reading.
+                reader.Close();
+            }
+        }
+
+        private static void ReadSingleRow(IDataRecord dataRecord)
+        {
+            Console.WriteLine(String.Format("{0}, {1}", dataRecord[0], dataRecord[1]));
+        }
+    }
+}
